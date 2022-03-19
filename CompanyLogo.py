0b8920027428c0ee489a3e6d9a0cd9cbdf7d19b9
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-19 13:56:45 +0300

    not working

--- CompanyLogo.py
+++ CompanyLogo.py
@@ -9,4 +9,11 @@ import sys
 if __name__ == '__main__':
     s = input("enter string: ")
     char_array = list(s)
+    keep_number: list
+
+    for item in range(0, len(char_array)-1):
+        if char_array[item] == char_array[item+1]: keep_number[item]
+        print(keep_number[item])
+
+
     print(char_array)
