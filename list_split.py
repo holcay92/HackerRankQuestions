67d1ec6bd25bceec6197b371966c4d4232edb75a
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-06-08 02:07:04 +0300

    list split example added

---
+++ list_split.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    N = int(input())
+    result :list()=[]
+    for _ in range(N):
+        operation = input().split()
+        if operation[0] == "print": print(result)
+        if operation[0] == "append": result.append(int(operation[1]))
+        if operation[0] == "insert": result.insert(int(operation[1]), int(operation[2]))
+        if operation[0] == "remove": result.remove(int(operation[1]))
+        if operation[0] == "pop": result.pop()
+        if operation[0] == "sort": result.sort()
+        if operation[0] == "reverse": result.reverse()
+
+
